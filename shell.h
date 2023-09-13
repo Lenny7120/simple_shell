6713b0be6f44d035c5c6ed5c820b1474907a2ffa
Lenny7120 <ocrank20@gmail.com>
2023-09-13 03:56:28 -0700

    simple shell

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+
+
+
+
+#endif
