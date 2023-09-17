2b4c5945ba2df193253c6c3e6326e0cd8d7ed017
Richie911 <oohrichmond.k@gmail.com>
2023-09-17 20:07:48 +0000

    Create Main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * main - run a simple shell
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+    if (isatty(STDIN_FILENO) == 1)
+    {
+        interactive();
+    }
+    else
+    {
+        not_interactive();
+    }
+    return 0;
+}
