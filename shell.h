b0cf4f4b99812a4a9b03b20e73f8a2fc597ec61d
Richie911 <oohrichmond.k@gmail.com>
2023-09-17 20:07:21 +0000

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,7 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,8 +8,26 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
+void interactive(void);
+char *readline(void);
+char **splitTerminalArgs(char *termArgs);
+int _execvp(const char *file, char *const argv[]);
+int executeArgs(char **args);
+int set_new_process(char **args);
+void displayCurrentDirectory(void);
+char *read_stream(void);
+void not_interactive(void);
 
+extern char **environ;
 
+int custom_cd(char **args);
+int custom_exit(char **args);
+int custom_env(char **args);
+int custom_help(char **args);
+
+#define TOKEN_DELIM " \t\r\n\a\""
 
 #endif
