fc69b82f1394c14106fff3ba2fc2f5b0062d1c16
Lenny7120 <ocrank20@gmail.com>
2023-09-18 10:50:44 -0700

    updated env function

--- custom_env.c
+++ custom_env.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * own_env - function that prints enviroment variables
+ * custom_env - function that prints enviroment variables
  * @args: arguments
  *
  * Return: 1 on success, 0 otherwise
  */
-int own_env(char **args)
+int custom_env(char **args)
 {
     int i = 0;
     (void)(**args);
