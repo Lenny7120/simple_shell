f237f0b873311018abd98088bc578e9c4e786708
Richie911 <oohrichmond.k@gmail.com>
2023-09-19 15:49:15 +0000

    Change DOS for new_process

--- new_process.c
+++ new_process.c
@@ -7,27 +7,28 @@
  */
 int set_new_process(char **args)
 {
-pid_t pid;
-int status;
+    pid_t pid;
+    int status;
 
-pid = fork();
-if (pid == 0)
-{
-	if (_execvp(args[0], args) == -1)
-	{
-		perror("error in set_new_process: child process");
-	}
-	exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-{
-	perror("error in set_new_process: forking");
-}
-else
-{
-	do {
-		waitpid(pid, &status, WUNTRACED);
-	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-}
-return (-1);
+    pid = fork();
+    if (pid == 0)
+    {
+        if (_execvp(args[0], args) == -1)
+        {
+            perror("error in set_new_process: child process");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        perror("error in set_new_process: forking");
+    }
+    else
+    {
+        do
+        {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return (-1);
 }
