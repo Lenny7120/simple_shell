7f994c947d8712449b14791ea16b97f26a6538c0
Richie911 <oohrichmond.k@gmail.com>
2023-09-20 00:02:47 +0000

    update main.c

--- main.c
+++ main.c
@@ -1,19 +1,114 @@
-#include "shell.h"
-/**
-* main - run a simple shell
-*
-* Return: 0
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
+void execute_command(const char *command);
+
+/**
+ * main - Entry point of the shell program.
+ *
+ * Description: Reads user input, executes commands, and exits o"exit" command.
+ *
+ * Return: Always returns 0.
+ */
 int main(void)
 {
-if (isatty(STDIN_FILENO) == 1)
-{
-interactive();
+    char input[BUFFER_SIZE];
+
+    while (1)
+    {
+        printf("$ ");
+        fgets(input, BUFFER_SIZE, stdin);
+
+        input[strlen(input) - 1] = '\0';
+
+        if (strcmp(input, "exit") == 0)
+        {
+            printf("Exiting the shell...\n");
+            break;
+        }
+        else if (strncmp(input, "exit ", 5) == 0)
+        {
+            /* Handle exit with status code */
+            int status_code = atoi(input + 5);
+
+            printf("Exiting the shell with status code: %d\n", status_code);
+            exit(status_code);
+        }
+
+        execute_command(input);
+    }
+
+    return (0);
 }
-else
+
+/**
+ * execute_command - Execute a given command.
+ *
+ * @command: The command to execute.
+ *
+ * Description: Forks a child process to execute the commandand waits for it to
+ * complete.
+ */
+void execute_command(const char *command)
 {
-not_interactive();
-}
-return (0);
+    pid_t pid = fork();
+    char *executable, *full_path, *full_command;
+    char *env[] = {NULL};
+
+    if (pid == -1)
+    {
+        perror("Fork failed");
+    }
+    else if (pid == 0)
+    {
+        /* child process*/
+        char *args[64];
+        char *token = strtok((char *)command, " ");
+        int i = 0;
+
+        while (token != NULL)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;
+
+        /* Specify the full path to the executable */
+        executable = args[0];
+        full_path = "/bin/"; /* Adjust this path as needed */
+        full_command =
+            malloc(strlen(full_path) + strlen(executable) + 1);
+        if (full_command == NULL)
+        {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        strcpy(full_command, full_path);
+        strcat(full_command, executable);
+
+        /* Prepare environment variables (optional) */
+
+        if (execve(full_command, args, env) == -1)
+        {
+            perror("Command execution failed");
+            exit(EXIT_FAILURE);
+        }
+
+        free(full_command);
+    }
+    else
+    {
+        /* Parent process */
+        int status;
+
+        waitpid(pid, &status, 0);
+    }
 }
